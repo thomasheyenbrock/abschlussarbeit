7740b9379e272b0e1c15fd869f1d5ab86f7ad97e
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2018-01-03 15:47:25 +0100

    change variable name

--- r/multipleLinearRegression.R
+++ r/multipleLinearRegression.R
@@ -1,6 +1,6 @@
 data <- read.csv2("./data/sample.csv", sep = ",", header = TRUE)
 
 modell <- as.formula("money ~ purchases + age")
-logit <- lm(modell, data = data)
+mlr <- lm(modell, data = data)
 
-print(logit)
+print(mlr)

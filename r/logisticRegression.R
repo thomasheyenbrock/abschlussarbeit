fb1054d2078c0dc1fd513b13754349f474fbdc51
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2018-01-10 13:41:47 +0100

    add flags for plot generation

--- r/logisticRegression.R
+++ r/logisticRegression.R
@@ -1,16 +1,26 @@
 args = commandArgs(trailingOnly = TRUE)
 
-n = 100000
-if (length(args) > 0) {
-    n = strtoi(args[1])
+n <- 100000
+plot <- TRUE
+if (length(args) == 1) {
+    if (substr(args[1], 1, 1) == "-") {
+        plot <- FALSE
+    } else {
+        n = strtoi(args[1])
+    }
+}
+if (length(args) == 2) {
+    if (substr(args[1], 1, 1) == "-") {
+        n = strtoi(args[2])
+    } else {
+        n = strtoi(args[1])
+    }
+    plot <- FALSE
 }
 
 start_time <- Sys.time()
 
 data <- head(read.csv2("./data/sample.csv", sep = ",", header = TRUE), n)
-
-xmin <- min(data$money)
-xmax <- max(data$money)
 
 modell <- as.formula("prime ~ money")
 logit <- glm(modell, family = binomial, data = data)
@@ -20,32 +30,37 @@ end_time <- Sys.time()
 print(logit)
 print(end_time - start_time)
 
-logitFunction <- function(x){
-    b0 <- coef(logit["coefficients"])[1]
-    b1 <- coef(logit["coefficients"])[2]
-    c <- b0 + x * b1
-    return(exp(c) / (1 + exp(c)))
+if (plot) {
+    xmin <- min(data$money)
+    xmax <- max(data$money)
+
+    logitFunction <- function(x){
+        b0 <- coef(logit["coefficients"])[1]
+        b1 <- coef(logit["coefficients"])[2]
+        c <- b0 + x * b1
+        return(exp(c) / (1 + exp(c)))
+    }
+
+    xplot <- seq(xmin - 1, xmax + 1, 1000)
+    yplot <- logitFunction(xplot)
+
+    plot(
+        c(xmin - 1, xmax + 1),
+        c(-0.2, 1.2),
+        type = "n",
+        xlab = "money",
+        ylab = "prime",
+        main = "Logistische Regression"
+    )
+    lines(
+        data$money,
+        data$prime,
+        type="p"
+    )
+    lines(
+        xplot,
+        yplot,
+        col = "red",
+        lwd = 2
+    )
 }
-
-xplot <- seq(xmin - 1, xmax + 1, 1000)
-yplot <- logitFunction(xplot)
-
-plot(
-    c(xmin - 1, xmax + 1),
-    c(-0.2, 1.2),
-    type = "n",
-    xlab = "money",
-    ylab = "prime",
-    main = "Logistische Regression"
-)
-lines(
-    data$money,
-    data$prime,
-    type="p"
-)
-lines(
-    xplot,
-    yplot,
-    col = "red",
-    lwd = 2
-)
--- r/simpleLinearRegression.R
+++ r/simpleLinearRegression.R
@@ -1,19 +1,26 @@
 args = commandArgs(trailingOnly = TRUE)
 
-n = 100000
-if (length(args) > 0) {
-    n = strtoi(args[1])
+n <- 100000
+plot <- TRUE
+if (length(args) == 1) {
+    if (substr(args[1], 1, 1) == "-") {
+        plot <- FALSE
+    } else {
+        n = strtoi(args[1])
+    }
+}
+if (length(args) == 2) {
+    if (substr(args[1], 1, 1) == "-") {
+        n = strtoi(args[2])
+    } else {
+        n = strtoi(args[1])
+    }
+    plot <- FALSE
 }
 
 start_time <- Sys.time()
 
 data <- head(read.csv2("./data/sample.csv", sep = ",", header = TRUE), n)
-
-xmin <- min(data$purchases)
-xmax <- max(data$purchases)
-
-ymin <- min(data$money)
-ymax <- max(data$money)
 
 modell <- as.formula("money ~ purchases")
 slr <- lm(modell, data = data)
@@ -23,30 +30,38 @@ end_time <- Sys.time()
 print(slr)
 print(end_time - start_time)
 
-b0 <- coef(slr["coefficients"])[1]
-b1 <- coef(slr["coefficients"])[2]
+if (plot) {
+    xmin <- min(data$purchases)
+    xmax <- max(data$purchases)
 
-xplot <- c(xmin - 1, xmax + 1)
-yplot <- c(b0 + (xmin - 1) * b1, b0 + (xmax + 1) * b1)
+    ymin <- min(data$money)
+    ymax <- max(data$money)
 
-plot(
-    c(xmin - 1, xmax + 1),
-    c(ymin - 1, ymax + 1),
-    type = "n",
-    xlab = "purchases",
-    ylab = "money",
-    main = "Einfache lineare Regression",
-    sub = paste("money = ", b0, "+", b1, "* purchases"),
-    col.sub = "darkgray"
-)
-lines(
-    data$purchases,
-    data$money,
-    type="p"
-)
-lines(
-    xplot,
-    yplot,
-    col = "red",
-    lwd = 2
-)
+    b0 <- coef(slr["coefficients"])[1]
+    b1 <- coef(slr["coefficients"])[2]
+
+    xplot <- c(xmin - 1, xmax + 1)
+    yplot <- c(b0 + (xmin - 1) * b1, b0 + (xmax + 1) * b1)
+
+    plot(
+        c(xmin - 1, xmax + 1),
+        c(ymin - 1, ymax + 1),
+        type = "n",
+        xlab = "purchases",
+        ylab = "money",
+        main = "Einfache lineare Regression",
+        sub = paste("money = ", b0, "+", b1, "* purchases"),
+        col.sub = "darkgray"
+    )
+    lines(
+        data$purchases,
+        data$money,
+        type="p"
+    )
+    lines(
+        xplot,
+        yplot,
+        col = "red",
+        lwd = 2
+    )
+}
